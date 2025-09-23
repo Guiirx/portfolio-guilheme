153f6c10236dfa897dfc1abfd03b77237f696a75
guilherme <2daygui@gmail.com>
2025-09-22 22:44:36 -0300

    adicionando a aba contact

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Profile } from '../models/profile';
+import { ProfileService } from '../services/profile';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,12 @@ import { Profile } from '../models/profile';
   styleUrl: './home.css'
 })
 export class Home {
- 
+
+  constructor(private profileService: ProfileService) { }
+
+  profile?: Profile;
+  
+  ngOnInit() {
+    this.profile = this.profileService.getProfile();
+  }
 }
--- src/app/services/profile.ts
+++ src/app/services/profile.ts
@@ -13,7 +13,7 @@ export class ProfileService {
     var profile: Profile = {
       email: 'yasmin.cabral_@hotmail.com',
       name: 'Yasmin Cabral',
-      whatsApp: '',
+      whatsApp: 'https://api.whatsapp.com/send?phone=5583986780591&text=Ol%C3%A1,%20estou%20no%20seu%20site%20%C3%A9%20gostaria%20de%20me%20consultar',
       instagram: ''
     }
 
